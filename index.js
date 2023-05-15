fa8b22f1eb0e7529c3381d7ae77459a13bb38fa9
systemtauqeer <systemtauqeer@gmail.com>
2023-05-15 11:28:34 +0500

    Resolve CORS Error

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require('cors');
 require("./config/database_connection");
 
 const users_route = require("./routes/users_routes");
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 
